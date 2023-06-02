50a106df152ec70be1f6aa75da3ec2c089498dde
Axrich <116589250+Axrich@users.noreply.github.com>
2023-06-03 00:12:57 +0500

    Update FormulaCreator.java

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,14 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int c = 3;
+        int d = 2;
+        int e = 7;
+
+        int result = a + b * c - d / e;
+
+        System.out.println("The result of the formula is: " + result);
     }
 }
